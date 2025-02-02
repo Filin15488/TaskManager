5338a3d062dd0864db514506156c84daaebd3ff9
Filin15488 <filya13579@yandex.ru>
2025-02-02 17:20:36 +0300

    fix Auth & Admin controllers docs in swagger

--- src/main/java/by/Filin/TaskManager/controler/AdminController.java
+++ src/main/java/by/Filin/TaskManager/controler/AdminController.java
@@ -3,6 +3,13 @@ package by.Filin.TaskManager.controler;
 import by.Filin.TaskManager.DTO.user.UserDTO;
 import by.Filin.TaskManager.service.AdminService;
 import by.Filin.TaskManager.service.CustomUserDetailsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.logging.Logger;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +22,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Tag(name = "AdminController", description = "API для администрирования")
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class AdminController {
 
@@ -24,6 +33,13 @@ public class AdminController {
 
     private final AdminService adminService;
 
+    @Operation(summary = "Назначение роли АДМИНИСТРАТОР пользователю",
+            description = "Назначает пользователю с указанным ID роль АДМИНИСТРАТОР. Доступно только пользователям с ролью АДМИНИСТРАТОР.",
+            security = @SecurityRequirement(name = "bearerAuth"),
+            responses = {
+                    @ApiResponse(responseCode = "202", description = "Роль АДМИНИСТРАТОР успешно назначена", content = @Content),
+                    @ApiResponse(responseCode = "403", description = "Запрещено - недостаточно прав", content = @Content)
+            })
     @PutMapping("/set/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> setAdmin(@PathVariable("id") Long id){
@@ -33,6 +49,16 @@ public class AdminController {
         return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
 
+    @Operation(summary = "Получение списка пользователей",
+            description = "Возвращает список всех пользователей. Доступно только пользователям с ролью АДМИНИСТРАТОР.",
+            security = @SecurityRequirement(name = "bearerAuth"),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Список пользователей успешно получен",
+                            content = @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = UserDTO.class)))
+                    ),
+                    @ApiResponse(responseCode = "403", description = "Запрещено - недостаточно прав", content = @Content)
+            })
     @GetMapping("/users")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<UserDTO>> getAllUsers(){
--- src/main/java/by/Filin/TaskManager/controler/AuthController.java
+++ src/main/java/by/Filin/TaskManager/controler/AuthController.java
@@ -1,15 +1,25 @@
 package by.Filin.TaskManager.controler;
 
 import by.Filin.TaskManager.DTO.jwt.*;
+import by.Filin.TaskManager.DTO.user.UserDTO;
 import by.Filin.TaskManager.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "AuthController", description = "Контроллер аутентификации пользователей")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -21,20 +31,59 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @Operation(summary = "Вход пользователя",
+            description = "Аутентифицирует пользователя и возвращает токены.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Успешный вход", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserDTO.class)),
+                            headers = {
+                                    @Header(name = "Set-Cookie", description = "Куки с токенами: access_token, refresh_token"),
+                                    @Header(name = "Access-Token", description = "Кастомный заголовок с access-токеном"),
+                                    @Header(name = "Refresh-Token", description = "Кастомный заголовок с refresh-токеном")
+                            }
+                    ),
+                    @ApiResponse(responseCode = "403", description = "Ошибка аутентификации - неверные учетные данные", content = @Content)
+            })
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthRequest authRequest, HttpServletResponse response) {
 //        authService.login(authRequest, response);
         return ResponseEntity.ok().body(authService.login(authRequest, response));
     }
 
+    @Operation(summary = "Обновление токенов аутентификации",
+            description = "Генерирует новые токены аутентификации с использованием refresh-токена.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Токены успешно обновлены",
+                            content = @Content,
+                            headers = {
+                                    @Header(name = "Set-Cookie", description = "Куки с токенами: access_token, refresh_token"),
+                                    @Header(name = "Access-Token", description = "Кастомный заголовок с access-токеном"),
+                                    @Header(name = "Refresh-Token", description = "Кастомный заголовок с refresh-токеном")
+                            }
+
+                    ),
+                    @ApiResponse(responseCode = "403", description = "Ошибка аутентификации - неверный или просроченный refresh-токен", content = @Content)
+            })
     @PostMapping("/refresh")
     public ResponseEntity<?> refresh(@CookieValue(value = "refresh_token", required = true) String refreshToken, HttpServletResponse response) {
         authService.refreshTokens(refreshToken, response);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Регистрация пользователя",
+            description = "Регистрирует нового пользователя с ролью ПОЛЬЗОВАТЕЛЬ и возвращает токены аутентификации.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Пользователь успешно зарегистрирован",
+                            headers = {
+                                    @Header(name = "Set-Cookie", description = "Куки с токенами: access_token, refresh_token"),
+                                    @Header(name = "Access-Token", description = "Кастомный заголовок с access-токеном"),
+                                    @Header(name = "Refresh-Token", description = "Кастомный заголовок с refresh-токеном")
+                            },
+                            content = @Content
+                    ),
+                    @ApiResponse(responseCode = "403", description = "Ошибка запроса - некорректные данные", content = @Content)
+            })
     @PostMapping("/registration")
-    public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest, HttpServletResponse response) {
+    public ResponseEntity<?> register(@RequestBody @Valid RegisterRequest registerRequest, HttpServletResponse response) {
         authService.register(registerRequest, response);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
